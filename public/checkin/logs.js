cf372e88a17540ab4809b76d5152104c244f1cee
Elliott Berger <s.bergellio@gmail.com>
2020-02-26 16:34:41 -0700

    Update logs.js

--- public/checkin/logs.js
+++ public/checkin/logs.js
@@ -1,7 +1,6 @@
 const mymap = L.map('checkinMap').setView([0, 0], 1);
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-// const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tileUrl = 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(mymap);
